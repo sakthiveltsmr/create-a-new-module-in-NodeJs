0d09e78d59715928d6be8cdf81976c9cb8ae8633
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-02-24 10:25:16 +0530

    calculation

---
+++ main.js
@@ -0,0 +1,9 @@
+var calculation = require("./math");
+
+var x = 55;
+var y = 10;
+
+console.log("add: " + calculation.add(x, y));
+console.log("sub: " + calculation.sub(x, y));
+console.log("mul: " + calculation.mul(x, y));
+console.log("div: " + calculation.div(x, y));
